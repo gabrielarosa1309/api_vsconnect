32bbf6f6c15cc419f133c2429e6abfce4278e830
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-10-27 21:54:37 -0300

    :tada: Início da API VsConnect

---
+++ src/main/java/com/senai/apivsconnect/services/FileUploadService.java
@@ -0,0 +1,41 @@
+package com.senai.apivsconnect.services;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class FileUploadService {
+    private final Path diretorioImg = Paths.get(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\img");
+    public String FazerUpload(MultipartFile imagem) throws IOException {
+        if (imagem.isEmpty()) {
+            System.out.println("Imagem vazia");
+            return null;
+        }
+
+        String nomeOriginal = imagem.getOriginalFilename();
+        String[] nomeArquivoArray = nomeOriginal.split("\\.");
+
+        //jpg
+        String extensaoArquivo = nomeArquivoArray[nomeArquivoArray.length - 1];
+
+        String prefixoArquivo = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyyHHmmss"));
+
+        String novoNomeImg = prefixoArquivo + "." + extensaoArquivo;
+
+        File imagemCriada = new File(diretorioImg + "\\" + novoNomeImg);
+
+        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(imagemCriada));
+
+        stream.write(imagem.getBytes());
+        stream.close();
+
+        return novoNomeImg;
+    }
+}
